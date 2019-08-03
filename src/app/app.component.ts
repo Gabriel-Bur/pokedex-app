eccc7534bc565da52571f381427a61b782042d8b
Gabriel Bur <gabriel.bur@outlook.com>
2019-08-02 23:48:11 -0300

    testing with mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/class/Pokemon';
+import { POKEMONSMOCK } from 'src/assets/pokemon-mock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pokedex-app';
+export class AppComponent implements OnInit{
+  Pokemons:Pokemon[];
+  title = 'Pokedex';
+
+constructor() {
+  this.Pokemons = POKEMONSMOCK;
+}
+
+  ngOnInit() {
+
+  }
 }
